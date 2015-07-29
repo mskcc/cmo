0c12f493a749c7018f69c9b1ea91093c01558c77
Chris Harris <ccharris@gmail.com>
2015-07-29 09:33:16 -0400

    bwa_sampe complex example

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mskcmo',
+        version='0.1',
+        description='Center for Molecular Oncology Python Package for Great Convenience and Enhanced Double Plus User Experiences',
+        url='github.com/mskcc/mskcmo.git',
+        author='Chris Harris',
+        author_email='harrisc2@mskcc.org',
+        licence='GPLv3',
+        packages=['mskcmo'],
+        install_requires=['argparse'],
+        scripts=['bin/bwa_sampe'],
+        zip_safe=False)
+
