e04c4b14a81d0c2e6ec500dabeda3cacef9a1021
Chris Harris <ccharris@gmail.com>
2015-07-31 10:31:16 -0400

    fix mskcmo --> cmo

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='mskcmo',
+setup(name='cmo',
         version='0.1',
         description='Center for Molecular Oncology Python Package for Great Convenience and Enhanced Double Plus User Experiences',
         url='github.com/mskcc/mskcmo.git',
         author='Chris Harris',
         author_email='harrisc2@mskcc.org',
         licence='GPLv3',
-        packages=['mskcmo'],
+        packages=['cmo'],
         install_requires=['argparse'],
         scripts=['bin/bwa_sampe',
                  'bin/bwa_align_paired_end',
