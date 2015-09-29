5422ba5ccd65a95e57d6fa35ca802e521f7abdda
Chris Harris <ccharris@gmail.com>
2015-09-29 15:41:47 -0400

    more jenkins build script fixes

--- jenkins_build_test.sh
+++ jenkins_build_test.sh
@@ -1,15 +1,14 @@
 PYENV_HOME=$WORKSPACE/.pyenv/
-
+PYTHON_HOME=/opt/common/CentOS_6-dev/python/python-2.7.10/bin/virtualenv
 # Delete previously built virtualenv
 if [ -d $PYENV_HOME ]; then
         rm -rf $PYENV_HOME
 fi
 
     # Create virtualenv and install necessary packages
-    virtualenv --no-site-packages $PYENV_HOME
+    $PYTHON_HOME/virtualenv --no-site-packages $PYENV_HOME
     . $PYENV_HOME/bin/activate
-    pip install --quiet nosexcover
-    pip install --quiet pylint
-    pip install --quiet $WORKSPACE/  # where your setup.py lives
+    $PYTHON_HOME/pip install --quiet nosexcover
+    $PYTHON_HOME/pip install --quiet $WORKSPACE/  # where your setup.py lives
     nosetests --with-xunit
 #    pylint -f parseable myapp/ | tee pylint.out
