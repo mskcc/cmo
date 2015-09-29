126be33c5bdcefdf0405d47b3b62557b310d6728
Chris Harris <ccharris@gmail.com>
2015-09-29 16:00:09 -0400

    support virtualenv

--- test/wrappers_print_help_test.py
+++ test/wrappers_print_help_test.py
@@ -7,7 +7,11 @@ def test_bin_scripts():
         yield check_for_help, os.path.join(bin_dir, file)
 
 def check_for_help(file):
-    proc = subprocess.Popen(["/opt/common/CentOS_6-dev/python/python-2.7.10/bin/python", file,"-h"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if hasattr(sys, 'real_prefix'): #we're in a virtualenv
+        python = "python"
+    else:
+        python = "/opt/common/CentOS_6-dev/python/python-2.7.10/bin/python"
+    proc = subprocess.Popen([python,  file,"-h"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     [stdout, stderr] = proc.communicate()
     if stderr.find("usage:") > -1:
         pass
