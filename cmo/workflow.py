2dfaa5b915728208cd059a7c5e14f8045d933850
lordzappo <ccharris@gmail.com>
2017-05-26 08:39:25 -0400

    Minor update, deploy fillout, workflow.py time delay, split_reads gzip tweaks for improved speed (#25)
    * helper script to index at start of module3..could be use for all modules
    
    * increment version for cmo_index, etc
    
    * initial commit of cmo_index
    
    * logging
    
    * cmo_split_reads now opens and emits gzips..slow bc native python gzip instead of subprocess zcat
    
    * add logger function
    
    * use bufferereader for speed improvement on gzip
    
    * increment version
    
    * version increment AGAIN
    
    * install cmo_fillout
    
    * longer delay for polling
    
    * speed improvement for split_reads...hopefully...
    
    * increment version for release

--- cmo/workflow.py
+++ cmo/workflow.py
@@ -163,7 +163,7 @@ class Workflow():
                 launcher_log_dir = os.path.join(FW_WFLOW_LAUNCH_LOC, getpass.getuser(), "")
                 queue_launcher.rapidfire(self.launchpad, fireworks.FWorker(name="LSF"), common_adapter, reserve=True, nlaunches=0, launch_dir=launcher_log_dir, sleep_time=10, njobs_queue=500)
                 failed_fws = []
-                time.sleep(10)
+                time.sleep(50)
 #                offline_runs =  self.launchpad.offline_runs.find({"completed": False, "deprecated": False}, {"launch_id": 1}).count()
 #                self.launchpad.m_logger.info("%s offline runs found" % offline_runs)
                 ready_lsf_jobs = self.launchpad.fireworks.find({"state":"READY", "spec._fworker" : "LSF"}).count()
