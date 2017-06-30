fb8bde00c6301799082e9efe625714ff33c68fb9
Chris Harris <ccharris@gmail.com>
2017-06-30 13:29:45 -0400

    dumbass tmpdir

--- cmo/gatk.py
+++ cmo/gatk.py
@@ -1,29 +1,29 @@
 import os, sys
 from . import util
-
+logger = util.get_logger()
 
 
 
 class Gatk:
-    def __init__(self,version="default", java_version="default", java_args="-Xmx48g -Xms256m -XX:-UseGCOverheadLimit", temp_dir=None, mutect=False):
+    def __init__(self,version="default", java_version="default", java_args="-Xmx48g -Xms256m -XX:-UseGCOverheadLimit", temp_dir="/scratch", mutect=False):
         try:
             if mutect:
                 self.gatk_jar=util.programs["mutect"][version]
             else:
                 self.gatk_jar=util.programs["gatk"][version]
         except KeyError, e:
-            print >>sys.stderr, "Cannot find specified version of gatk in configuration file: %s" % version
+            logger.critical("Cannot find specified version of gatk in configuration file: %s" % version)
             sys.exit(1)
         try: 
             self.java_cmd=util.programs["java"][java_version]
         except KeyError, e:
-            print >>sys.stderr, "Cannot find specified version of java to run gatk with: %s" % java_version
+            logger.critical("Cannot find specified version of java to run gatk with: %s" % java_version)
             sys.exit(1)
         self.temp_dir=None
         if temp_dir:
             self.temp_dir = temp_dir
         self.java_args = java_args 
-        print "selected %s" % self.gatk_jar
+        logger.info("selected %s" % self.gatk_jar)
     def gatk_cmd(self, command, java_args_override=None, command_specific_args={}):
         cmd = [self.java_cmd, self.java_args]
         if(self.temp_dir != None):
